babe6da0b2701b7149f156628f8023b6b9b18aa8
Moonlization <seva2409@gmail.com>
2022-02-10 11:31:44 +0100

    grid

--- client/src/layouts/Mainlayout.js
+++ client/src/layouts/Mainlayout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
-export const Mainlayout = ( props ) => {
+export const Mainlayout = ( {children} ) => {
   return (
-    <React.Fragment>
-      <Navbar></Navbar>
-  <main>{props.children}</main>
-  </React.Fragment>
+    <>
+      <Navbar/>
+  <main>{children}</main>
+  </>
+  
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,49 @@
-import React from 'react'
 
+import { useEffect, useState } from 'react'
 export const Home = () => {
+const [data, setdata] = useState("")
+const [seconds, setseconds] = useState(0)
+
+
+useEffect(()=>{
+  const getData = ()=>{
+    const int = setInterval(()=>{
+      
+    
+     setseconds(prev => prev + 1)},1000)
+     setTimeout(()=>{
+      
+      
+   setdata('OK')
+   clearInterval(int)
+     },3000)
+   }
+ console.log('render2')
+  getData()
+  
+},[])
   return (
     <div className='home'>
-<div className="greet">
-  Hello!
+<div className="lastNews">
+  <div className="heading"></div>
+  <div className="description"></div>
 </div>
-
+<div className="greet">
+<div className="heading"></div>
+  <div className="description"></div>
+</div>
+<div className="greet">
+<div className="heading"></div>
+  <div className="description"></div>
+</div>
+<div className="greet">
+<div className="heading"></div>
+  <div className="description"></div>
+</div>
+<div className="greet">
+<div className="heading"></div>
+  <div className="description"></div>
+</div>
     
     </div>
   )
